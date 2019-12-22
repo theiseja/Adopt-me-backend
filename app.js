0ff35d686d57de29ade450ae0931301df36500c0
Jesse <theiseja@gmail.com>
2019-12-22 13:49:37 -0600

    Adds basic GET request

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const hostname = '127.0.0.1';
 const port = 3000; // will be changed later
 
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Welcome to Adopt me pet adoption!');
 });
 
-server.listen(port, hostname, () => {
+app.get('/test', function (req, res) {
+    res.send('Hello! :)')
+})
+
+app.listen(port, hostname, () => {
     console.log('Server running at http://${hostname}:${port}/');
 });
